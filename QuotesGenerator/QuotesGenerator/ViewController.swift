2c08e5b2ea22ea70bbf4d69a237dffbd4d5f0fdd
HeeWoong Lee <howllee@me.com>
2022-11-03 19:55:49 +0900

    Generate First Project & Push Test

---
+++ QuotesGenerator/QuotesGenerator/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  QuotesGenerator
+//
+//  Created by 이희웅 on 2022/11/03.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
